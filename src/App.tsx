80bda12de6447a1af676aec460cf29009c541a56
xDunkan <xdunkanx@mail.ru>
2021-11-26 16:55:22 +0600

    feat: boards

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,15 @@
 import React, { useState } from 'react';
 import './App.css';
-import { TodoList } from './components/TodoList';
-import { ITodo } from './types/data'
+import { Boards } from './components/boards';
 
 const App: React.FC = () => {
-  const [value, setValue] = useState('');
-  const [todos, setTodos] = useState<ITodo[]>([]);
-
-  const handleKeyDown: React.KeyboardEventHandler<HTMLInputElement> = (e) => {
-    if (e.key === 'Enter') addTodo();
-  }
-
-  const addTodo = () => {
-    if (value) {
-      setTodos([...todos, {
-        id: Date.now(),
-        title: value
-      }])
-      setValue('')
-    }
-  }
-
-  const removeTodo = (id: number): void => {
-    setTodos(todos.filter(todo => todo.id !== id))
-  };
 
   return (
     <div className="app">
-      <div>
-        <input value={value} onChange={e => setValue(e.target.value)} onKeyDown={handleKeyDown} />
-        <button onClick={addTodo}>Добавить</button>
-      </div>
-      <TodoList items={todos} removeTodo={removeTodo} />
+      <Boards />
+      <Boards />
+      <Boards />
+      <Boards />
     </div>
   );
 }
---
+++ src/components/boards.tsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import { TodoList } from './TodoList';
+import { ITodo } from '../types/data'
+
+const Boards: React.FC = () => {
+  const [value, setValue] = useState('');
+  const [todos, setTodos] = useState<ITodo[]>([]);
+
+  const handleKeyDown: React.KeyboardEventHandler<HTMLInputElement> = (e) => {
+    if (e.key === 'Enter') addTodo();
+  }
+
+  const addTodo = () => {
+    if (value) {
+      setTodos([...todos, {
+        id: Date.now(),
+        title: value
+      }])
+      setValue('')
+    }
+  }
+
+  const removeTodo = (id: number): void => {
+    setTodos(todos.filter(todo => todo.id !== id))
+  };
+
+	return <div className='app__board'>
+    <div>
+        <input value={value} onChange={e => setValue(e.target.value)} onKeyDown={handleKeyDown} />
+        <button onClick={addTodo}>Добавить</button>
+      </div>
+      <TodoList items={todos} removeTodo={removeTodo} />
+  </div>
+}
+
+export { Boards }
